0bee0ef3a42f8e20288774a09f9e386b5da644fc
Wellington Santana <90020289+Welldevbr@users.noreply.github.com>
2023-02-22 14:13:56 -0300

    Modified logo

--- src/components/Project/styles.ts
+++ src/components/Project/styles.ts
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-
+  > a {
+    position: relative;
+    width: 100%;
+    height: 15rem;
+    padding: 1.5rem;
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-start;
+    border-radius: 0.5rem;
+    border: 1px solid ${({ theme }) => theme.border};
+    cursor: pointer;
+    overflow: hidden;
+    transition: 0.5s;
+  }
 `;
